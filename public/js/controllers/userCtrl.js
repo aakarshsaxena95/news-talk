30875a8002f391587d651db7ba71523aeb8c7ae8
Ayush Gupta <ayushgp10@gmail.com>
2015-10-13 11:43:10 +0530

    Some progress in my Profile page. The recent reading list and comments front end working

--- public/js/controllers/userCtrl.js
+++ public/js/controllers/userCtrl.js
@@ -1,6 +1,36 @@
 angular.module("newstalk")
 	.controller("UserController",["$scope","$http",function($scope,$http){
-		$http.get('/api/readinglistlinks').then(function(response){
-			console.log(response);
-		});
+		$scope.readinglist = [];
+		$http.get('/api/readinglistlinks')
+			.then(function(response){
+				console.log("ashfjsahd");
+			response.data.articles.forEach(function(article){
+				$scope.readinglist.push(article);
+			});
+			console.log($scope.readinglist);
+			});
+		
+		$scope.recentcomments = [];
+		//TODO: Modify this to call it for recent comments by the user
+		/*
+			COMMENT STRUCTURE:
+			{
+				user._id: string,
+				comment content(truncated),
+				comment on article link(on my page)
+			}
+		
+		*/
+//		$http.get('/api/commentsforprofile').then(function(response){
+//			response.data.comments.forEach(function(comment){
+//				$scope.recentcomments.push(comment);				
+//			});
+//		});
+				$scope.recentcomments.push({
+					content:'asfasyudfbsdah kuawsgfkuya gsdkf',
+					article:{
+						link:'skudhfksdf.com',
+						title:'sjdhfadfkjahgsdhbsd'
+					}
+				});
 	}]);
